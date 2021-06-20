64ee6890ec22967f85938a105ac6f4423af1d6f8
Mohammed1994Mosleh <mohammed1994mosleh@gmail.com>
2021-06-20 18:38:18 +0300

    add lab01b

---
+++ js/app.js
@@ -0,0 +1,53 @@
+// alert('hi')
+//string
+// var userName = prompt('what is your name');
+// console.log(userName);
+// var age = 35;//number
+// var grade = 75.5
+// var result = true;//boolean
+let Name=prompt('type your Name')
+let age=prompt('how old are you?')
+let faviorateMeal=prompt('do you brefer shwirma or munsuf?')
+let faviorateDrink=prompt('whats your faviorate drink ?')
+alert('hi '+Name)
+alert('we have most delecious  '+faviorateMeal)
+alert (Name +" have "+age+" likes "+faviorateMeal+" with "+faviorateDrink )
+console.log("Name: "+Name);
+console.log("age:"+age);
+console.log("faviorateMeal: "+faviorateMeal);
+console.log("faviorateDrink: "+faviorateDrink);
+console.log();
+
+
+/*var userAge = 35;
+var userAge = 25;
+console.log(userAge);
+
+
+grade = 100;
+var grade;
+console.log(grade);
+
+
+let userName = 'shihab';
+userName = 'ahmad';
+console.log(userName);
+
+// mark = 90;
+// let mark;
+// console.log(mark);
+
+const pie = 3.14;
+
+// pie = 3;
+
+console.log(pie);
+
+let str1 = 'shihab ';
+let str2 = 'eshtaiwi';
+console.log(str1 + str2);
+
+let num1 = 10;
+let num2 = '100';
+let num3 = num1 + num2;
+console.log(typeof num3);*/
